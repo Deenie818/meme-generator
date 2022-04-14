004cd25facf5a9ef6272d0fe5cb9f102fee2a960
djteiji <djteiji@gmail.com>
2022-04-13 17:52:00 -0700

    fix clear search function, fix layout

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,10 @@
 // var requestUrl = "https://www.flickr.com/services/feeds/photos_public.gne";
 var searchButton = document.querySelector(".searchbutton");
 var nextButton = document.querySelector(".next");
-var input = document.querySelector("input");
+var inputEl = document.querySelector("input");
+var dogResult = document.querySelector("#dog-result");
+var galleryEl = document.querySelector(".gallery")
+var searchFormEl = document.querySelector("#user-form")
 
 var pagenr = 1;
 var search = false;
@@ -21,11 +24,57 @@ var query = "";
 
 // // curatedPhotos(pagenr);
 
-var getPhotos = function() {
-    var response = fetch("https://picsum.photos/200");
-    console.log(response);
+var getPhotos = function(search) {
+    
+    fetch("https://cors-anywhere.herokuapp.com/https://imsea.herokuapp.com/api/1?q=" + search).then(function(response){
+        response.json().then(function(data){
+            console.log(data);  
 
+            for (var i =0; i < 60; i+= 2) {
 
+            var picSearchEl = document.createElement("div");
+            
+            picSearchEl.innerHTML = `<img src="${data.results[i]}"/>`
+
+            galleryEl.appendChild(picSearchEl);
+
+            
+
+            };
+
+            
+
+            // dogResult.innerHTML = `<img src="${data.results[5]}"/>`
+        });
+
+        
+    });
+    
 };
 
-getPhotos();
+
+var searchInputHandler = function (event) {
+    event.preventDefault();
+
+    clearQueue();
+    
+    var searchTerm = inputEl.value.trim();
+
+    if (searchTerm) {
+        getPhotos(searchTerm);
+
+
+        inputEl.value = "";
+    } else {
+        alert('Please enter a search term');
+      }
+}
+
+var clearQueue = function() {
+    // inputEl.value = "";
+    document.querySelector(".gallery").innerHTML = "";
+}
+
+searchFormEl.addEventListener("submit", searchInputHandler);
+
+// getPhotos();
