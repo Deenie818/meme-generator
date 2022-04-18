e1fb9cb0fff662a270f7edfe62f7b68931c560af
mahnameisdavid <dee.echeverrria@gmail.com>
2022-04-18 14:42:10 -0700

    now displayed in local storage

--- assets/js/my-memes.js
+++ assets/js/my-memes.js
@@ -1,77 +1,3 @@
-// 1st
-
-//var form = document.getElementById('form')
-
-//varparentDiv = document.getElementById('result')
-
-//form.addEventListener('submit', function (event) {
-    //event.preventDefault()
-
-    //var reader = new FileReader()
-
-    //var name = document.getElementById("imageFileInput").files[0].name
-
-    //reader.addEventListener('load', function () {
-        //if (this.result && localStorage) {
-           // window.localStorage.setItem(name, this.result)
-            //alert("image stored")
-          //  showImages()
-        //}
-        //else{
-          //  alert("unsuccessful")
-        //}
-   // })
-
-   // reader.readAsDataURL(document.getElementById('imageFileInput').files[0])
-
-    //console.log(name)
-//})
-
-//function showImages() {
-   // for (let i = 0; i < window.localStorage.lenght; i++){
-       // let res = window.localStorage.getItem(window.localStorage.key(i))
-       // var image = new Image()
-       // image.src = res;
-
-        //parent.dispatchEvent.appendChild
-    //}
-//}
-
-//showImages()
-
-
-//2nd
-
-//3rd
-
-//at the click of the button we can now be ready to generate button
-//myBtn.addEventListener("click", function() {
-  //  console.log(input.files)
-//});
-
-
-
-//document.querySelector("#imageFileInput").addEventListener("generate", function() {
-  //  const reader = new FileReader();
-
-    //reader.addEventListener("load", () => {
-      //  localStorage.setItem("recent-image", reader.result);
-    //});
-
-    //reader.readAsDataURL(this.files[0]);
-
-
-//});
-
-//document.addEventListener("DOMContentLoaded", () => {
-  //  const recentImaggeDataUrl = localStorage.getItem("recent-image");
-
-    //if (recentImaggeDataUrl) {
-      //  document.querySelector("#imgPreview").setAttribute("src", recentImageDataUrl);
-    //}
-
-//});
-
 let memes = [];
 
 const addMeme = (ev)=>{
@@ -86,8 +12,9 @@ const addMeme = (ev)=>{
     // this clears the form
     document.forms[0].reset();
 
-    //so the meme is then displayed
-    console.warn('added',{memes} );
+    
+    // now we will save the info to local storage
+    localStorage.setItem('MyMemeList', JSON.stringify(memes) );
     
 }
 
